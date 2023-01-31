a67a41b7275bb712a9946bf7779d609519e4593d
koneko <67551503+koneko@users.noreply.github.com>
2023-01-31 17:09:20 +0100

    added maps automatically (1675181360089)

---
+++ publish.js
@@ -0,0 +1,31 @@
+// import exec
+const { exec } = require('child_process');
+
+// automate the process of publishing to github repository
+
+exec("git add .", function (err, stdout, stderr) {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log(stdout);
+    }
+})
+
+exec(`git commit -m \"added maps automatically (${Date.now()})\"`, function (err, stdout, stderr) {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log(stdout);
+    }
+})
+
+// origin master
+exec("git push origin master", function (err, stdout, stderr) {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log(stdout);
+    }
+})
+
+console.log("Finished publishing maps, list will update in a few minutes.")
